471dc4531e35ff80c523ecd2688bfb1fd2733bc9
Divyansh Goyal <divyansh.goyal2001@gmail.com>
2022-02-23 15:00:23 -0700

    Added a template file

--- src/AUCCI/webapp/views.py
+++ src/AUCCI/webapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
-# Create your views here.
+# View Functions
+def index(request):
+    return render(request, 'index.html')
