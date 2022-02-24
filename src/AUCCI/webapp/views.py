9ae72052552a7fa94580536ad888ffebf1f27b72
Divyansh Goyal <divyansh.goyal2001@gmail.com>
2022-02-23 19:16:01 -0700

    Added register and login functionality

--- src/AUCCI/webapp/views.py
+++ src/AUCCI/webapp/views.py
@@ -5,4 +5,57 @@ from django.contrib import messages
 
 # View Functions
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        if request.POST.get("login_s"):
+            return redirect('login')
+        if request.POST.get("register_s"):
+            return redirect("register")
+    else:
+        return render(request, 'index.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "The Username and/or Password entered are incorrect!")
+            return redirect("login")
+    else:
+        return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        verifypass = request.POST['confirmpass']
+        email = request.POST['email']
+
+        if password == verifypass:
+            # User is the thing being imported, filter goes in the db and checks 
+            # if the email entered already exists.
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already in use! Please go back and log in!")
+                # Send em back to register
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username not available! Try a different one!")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, "Passwords did not match!")
+            return redirect('register')
+
+    else:
+        return render(request, 'register.html')
+
+def home(request):
+    return render(request, 'homepage.html')
