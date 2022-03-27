7660e2b23aaa539c392fea12e46031d7ae67cd9e
LiamParmar <81483239+LiamParmar@users.noreply.github.com>
2022-03-27 14:27:36 -0600

    deployment

--- src/AUCCI/AUCCI/settings.py
+++ src/AUCCI/AUCCI/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from .credentials import connections
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +141,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 LOGIN_URL = "login"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
