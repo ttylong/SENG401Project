e0919cc47bbd4794ca751ad9340ef35a638e123f
LiamParmar <81483239+LiamParmar@users.noreply.github.com>
2022-03-27 15:02:24 -0600

    got rid of credentials in git ignore

---
+++ src/AUCCI/AUCCI/credentials.py
@@ -0,0 +1,12 @@
+class connections:
+    def dbconnectionstring():
+        return "mongodb+srv://auccibids:Seng401!@aucci.eqyli.mongodb.net/aucciDB"
+
+    def djangokeystring():
+        return "django-insecure-_io%686yfof7@wx(5x#ag^e646+78mwkzj4y6bm#a-m(-qq)+p"
+
+    def uploadcarepub():
+        return "20a0df730e28f42bb662"
+
+    def uploadcarepriv():
+        return "8ad164c8ada8aaf4034f"
